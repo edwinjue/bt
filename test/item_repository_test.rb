63295e21fadd7828df32f4b1c978139c1b984a32
Alireza Andersen <alireza.andersen@gmail.com>
2016-01-11 21:57:44 -0700

    Item Repository added with tests

--- test/item_repository_test.rb
+++ test/item_repository_test.rb
@@ -29,6 +29,59 @@ class ItemRepositoryTest <  Minitest::Test
   def test_can_find_item_by_name
     ir = ItemRepository.new
     ir.load_data("./data/items.csv")
-    
+    item = ir.find_by_name("Disney scrabble frames")
+    assert_equal "Disney scrabble frames", item.name
+    assert_equal 263395721, item.id
+  end
+
+  def test_can_find_item_by_name_regardless_of_case
+    ir = ItemRepository.new
+    ir.load_data("./data/items.csv")
+    item = ir.find_by_name("Free standing Woden letters")
+    assert_equal "Free standing Woden letters", item.name
+    assert_equal 263396013, item.id
+  end
+
+  def test_can_find_all_by_description
+    skip
+    ir = ItemRepository.new
+    ir.load_data("./data/items.csv")
+    description = "Acrylique sur toile exécutée en 2004
+    Format : 50 x 60 cm
+    Toile sur châssis en bois - non encadré
+    Artiste : Flavien Couche - Artiste côté Akoun
+
+    TABLEAU VENDU AVEC FACTURE ET CERTIFICAT D&#39;AUTHETICITE
+
+    www.flavien-couche.com"
+    item = ir.find_by_description(description)
+    assert_equal description, item[0].description
+    assert_equal 263398427, item[0].id
+  end
+
+  def test_find_all_by_price
+    ir = ItemRepository.new
+    ir.load_data("./data/items.csv")
+    items = ir.find_all_by_price(4800)
+    assert_equal 4800, items[0].unit_price
+    assert_equal 7, items.length
+  end
+
+  def test_find_all_by_price_in_range
+    ir = ItemRepository.new
+    ir.load_data("./data/items.csv")
+    range = Range.new(13,150)
+    items = ir.find_all_by_price_in_range(range)
+    assert_equal 6, items.length
+  end
+
+  def test_find_all_by_merchant_ids
+    ir = ItemRepository.new
+    ir.load_data("./data/items.csv")
+    items = ir.find_all_by_merchant_id(12334442)
+    assert_equal 12334442, items[0].merchant_id
+    assert_equal 5, items.length
+  end
+
 
 end
